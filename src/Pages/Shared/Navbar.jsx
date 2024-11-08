3cc556bbaed88102e9e9bfe534d7bf2f48e741d0
bijoymamud <bijoymamud.09@gmail.com>
2024-11-08 16:47:14 +0600

    project setup

---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../../Layout/Container/Container'
+
+const Navbar = () => {
+  return (
+    <section>
+          <Container>
+              
+      </Container>
+    </section>
+  )
+}
+
+export default Navbar
